6faa9495396c77a42814e791ebbbf4a34b00d931
kino-ma <kino.ma.ms@gmail.com>
2021-06-10 17:22:29 +0900

    [refactor] add `compiler::parser` module

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,8 +1,1 @@
-#[cfg(test)]
-extern crate std;
-mod test {
-    #[test]
-    fn add_two_numbers() {
-        let ops = [0x41, 0x0a, 0x41, 0x14, 0x6a, 0x0b];
-    }
-}
+pub mod parser;
---
+++ src/compiler/parser.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+extern crate std;
+mod test {
+    #[test]
+    fn add_two_numbers() {
+        let ops = [0x41, 0x0a, 0x41, 0x14, 0x6a, 0x0b];
+        panic!()
+    }
+}
