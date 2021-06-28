6d059f882d5e956ebf6f064942de3e56ea7b98c1
kino-ma <kino.ma.ms@gmail.com>
2021-06-28 14:34:11 +0900

    [feat] use wasm-parsing library

--- compiler/src/ir.rs
+++ compiler/src/ir.rs
@@ -1,91 +1,22 @@
-pub(crate) use alloc::vec::Vec;
-
-use num_derive::FromPrimitive;
+use parity_wasm::elements::Module as WasmModule;
 
 /// Intermidate representation of a WebAssembly Module
 #[cfg_attr(test, derive(Debug))]
 pub struct Module {
-    version: u32,
-    sections: Vec<Section>,
+    inner: WasmModule,
 }
 
 impl Module {
-    pub fn new((_magic, version, sections): (&[u8], u32, Vec<Section>)) -> Self {
+    pub fn new(module: WasmModule) -> Self {
         Self {
-            version,
-            sections,
+            inner: module,
         }
     }
-}
 
-/// Intermidate representation of a section
-#[cfg_attr(test, derive(Debug))]
-pub struct Section {
-    code: u8,
-    size: u32,
-    content: Vec<u8>,
-}
-
-impl Section {
-    pub fn new((code, size, content): (u8, u32, Vec<u8>)) -> Self {
-        Self {
-            code,
-            size,
-            content
-        }
-    }
-}
-
-#[derive(FromPrimitive)]
-pub enum SectionType {
-    Custom = 0,
-    Type = 1,
-    Import = 2,
-    Function = 3,
-    Table = 4,
-    Memory = 5,
-    Global = 6,
-    Export = 7,
-    Start = 8,
-    Element = 9,
-    Code = 10,
-    Data = 11,
-    DataCount = 12,
-}
-
-/// Intermidate representation
-pub struct IR<'a> {
-    /// Raw insturction before converted to IR
-    _raw_instruction: &'a [u8],
-    _typ: IRType,
-}
-
-/// What kind of entry
-pub enum IRType {
-    I32,
-    I64,
-    F32,
-    F64,
-}
-
-impl<'a> IR<'a> {
-    pub fn new(raw_instruction: &'a [u8]) -> Self {
-        Self {
-            _raw_instruction: raw_instruction,
-            _typ: IRType::I32,
-        }
-    }
-}
-
-pub struct Function {
-
-}
-
-impl Function {
-    pub fn new() -> Self {
-        Self {
-
-        }
+    pub fn parse_buffer(buf: &[u8]) -> Result<Self, ()> {
+        let module = parity_wasm::deserialize_buffer(buf)
+            .or(Err(()))?;
+        Ok(Self::new(module))
     }
 }
 
--- compiler/src/parse.rs
+++ compiler/src/parse.rs
@@ -1,3 +1,9 @@
 extern crate parity_wasm;
 
+use crate::ir::Module;
+
 use num_traits::{Unsigned, NumCast};
+
+pub fn parse(buf: &[u8]) -> Result<Module, ()> {
+    Module::parse(buf)?;
+}
