52584632c56d5aaecc3042ce572dff22b2f81d49
kino-ma <kino.ma.ms@gmail.com>
2021-06-30 23:06:36 +0900

    [refactor] move `generate_func` to arch-specific mod

--- compile/src/codegen.rs
+++ compile/src/codegen.rs
@@ -1,15 +1,5 @@
-#[cfg(aarch64)]
+#[cfg(feature = "aarch64")]
 mod aarch64;
 
-#[cfg(aarch64)]
+#[cfg(feature = "aarch64")]
 pub use aarch64::*;
-
-use alloc::prelude::v1::*;
-
-use parity_wasm::elements::FuncBody;
-
-use crate::err::{Error::*, Result};
-
-pub fn generate_func(body: &FuncBody) -> Result<Vec<u8>> {
-    Err(Failure)
-}
--- compile/src/codegen/aarch64.rs
+++ compile/src/codegen/aarch64.rs
@@ -1,1 +1,9 @@
+use alloc::prelude::v1::*;
 
+use parity_wasm::elements::FuncBody;
+
+use crate::err::{Error::*, Result};
+
+pub fn generate_func(body: &FuncBody) -> Result<Vec<u8>> {
+    Err(Failure)
+}
