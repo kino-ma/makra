1c54e98b1a71e8a1b3f487559302f875abd0ed4f
kino-ma <kino.ma.ms@gmail.com>
2021-06-30 12:54:59 +0900

    [feat] add `Compiler` struct

--- compiler/src/compile.rs
+++ compiler/src/compile.rs
@@ -1,1 +1,13 @@
-pub struct Compiler();
+use crate::ir::Module;
+
+pub struct Compiler {
+    module: Module,
+}
+
+impl Compiler {
+    pub fn new(module: Module) -> Self {
+        Self {
+            module,
+        }
+    }
+}
