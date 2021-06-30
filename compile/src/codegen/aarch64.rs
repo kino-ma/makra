24fb2a183b02f8d03547ef1a7221ab690e1707a1
kino-ma <kino.ma.ms@gmail.com>
2021-07-01 00:35:33 +0900

    [docs]  todo

--- compile/src/codegen/aarch64.rs
+++ compile/src/codegen/aarch64.rs
@@ -5,5 +5,6 @@ use parity_wasm::elements::FuncBody;
 use crate::err::{Error::*, Result};
 
 pub fn generate_func(body: &FuncBody) -> Result<Vec<u8>> {
+    TODO test.wasmのアレを変換できるようにする
     Err(Failure)
 }
