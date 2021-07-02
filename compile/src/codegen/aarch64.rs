5094e6f90c8223ec82af8d4f169e563e5d69565e
kino-ma <kino.ma.ms@gmail.com>
2021-07-02 18:48:12 +0900

    [feat] add `pop` and `add`

--- compile/src/codegen/aarch64.rs
+++ compile/src/codegen/aarch64.rs
@@ -24,7 +24,6 @@ fn wasm2bin(inst: &Instruction) -> Result<Vec<Code>> {
     match inst {
         I32Const(x) => {
             let x = *x;
-            // 1110_101[S]_0000_[Reg; 4][#imm; 12]
             let mov_r0 = mov(0, x)?;
             let push_r0 = push(0)?;
             Ok(vec![mov_r0, push_r0])
@@ -33,7 +32,7 @@ fn wasm2bin(inst: &Instruction) -> Result<Vec<Code>> {
         I32Add => {
             let pop_1 = pop(1)?;
             let pop_2 = pop(2)?;
-            let add_ = add(1, 2)?;
+            let add_ = add(0, 1, 2)?;
             let push_r0 = push(0)?;
             Ok(vec![pop_1, pop_2, add_, push_r0])
         }
@@ -43,6 +42,7 @@ fn wasm2bin(inst: &Instruction) -> Result<Vec<Code>> {
 }
 
 fn mov(dist: u8, val: i32) -> Result<Code> {
+    // 1110_101[S]_0000_[Reg; 4][#imm; 12]
     if val > 1i32.wrapping_shl(11) {
         Err(TooLargeI32(val))
     } else {
@@ -53,8 +53,21 @@ fn mov(dist: u8, val: i32) -> Result<Code> {
     }
 }
 
+fn add(dist: u8, src_n: u8, src_m: u8) -> Result<Code> {
+    // 1110_00_0_0100_0_[src_n; 4]_[dist; 4]_[shift; 5]_00_0_[src_m; 4]
+    Ok((0xe080_0000_u32
+        | src_n.wrapping_shl(15) as u32
+        | dist.wrapping_shl(11) as u32
+        | src_m as u32)
+        .to_le_bytes())
+}
+
 fn push(src: u8) -> Result<Code> {
     Ok(to_le([0xe5, 0x2d, src << 4, 0x04]))
+}
+
+fn pop(dist: u8) -> Result<Code> {
+    Ok(to_le([0xe4, 0x9d, dist << 4, 0x04]))
 }
 
 fn to_le(mut code: Code) -> Code {
