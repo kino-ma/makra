36775bcbfa7f70bc9db4919f3482cb164fa718f7
kino-ma <kino.ma.ms@gmail.com>
2021-07-02 15:57:05 +0900

    [feat] add `wasm2bin()` and arm binaries for testcase

--- compile/src/codegen/aarch64.rs
+++ compile/src/codegen/aarch64.rs
@@ -1,10 +1,14 @@
 use alloc::prelude::v1::*;
 
-use parity_wasm::elements::FuncBody;
+use parity_wasm::elements::{FuncBody, Instruction};
 
 use crate::err::{Error::*, Result};
 
 pub fn generate_func(body: &FuncBody) -> Result<Vec<u8>> {
-    TODO test.wasmのアレを変換できるようにする
+    //TODO test.wasmのアレを変換できるようにする
     Err(Failure)
+}
+
+fn wasm2bin(inst: Instruction) -> [u8; 4] {
+    [0; 4]
 }
