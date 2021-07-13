596aa09dbb6622fcd069c1960937efd0da604e1a
kino-ma <kino.ma.ms@gmail.com>
2021-07-13 16:59:43 +0900

    [feat] add evaluation program

---
+++ evaluation/prime.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n = 2147483647;
+
+    let mut i = 1;
+    loop {
+        i += 1;
+        if n % i == 0 || i == n {
+            break;
+        }
+    }
+
+    println!("result: {}", n - i == 0);
+}
