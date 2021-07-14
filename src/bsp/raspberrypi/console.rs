02c1d314639f0965b27e6c4127ad9269f4a9b9c3
kino-ma <kino.ma.ms@gmail.com>
2021-07-14 13:23:51 +0900

    [refactor] remove unused `QEMUOutput`

--- src/bsp/raspberrypi/console.rs
+++ src/bsp/raspberrypi/console.rs
@@ -3,28 +3,6 @@ use core::fmt;
 use crate::{bsp::device_driver, console};
 use super::memory;
 
-/// A mystical, magical device for generating QEMU output out of the void.
-struct QEMUOutput;
-
-/// Implementing `core::fmt::Write` enables usage of the `format_args!` macros, which in turn are
-/// used to implement the `kernel`'s `print!` and `println!` macros. By implementing `write_str()`,
-/// we get `write_fmt()` automatically.
-///
-/// See [`src/print.rs`].
-///
-/// [`src/print.rs`]: ../../print/index.html
-impl fmt::Write for QEMUOutput {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
-            unsafe {
-                core::ptr::write_volatile(0x3F20_1000 as *mut u8, c as u8);
-            }
-        }
-
-        Ok(())
-    }
-}
-
 pub unsafe fn panic_console_out() -> impl fmt::Write {
     let mut panic_gpio = device_driver::PanicGPIO::new(memory::map::mmio::GPIO_START);
     let mut panic_uart = device_driver::PanicUart::new(memory::map::mmio::PL011_UART_START);
